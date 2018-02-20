1282f433e72e3dd0cc2f4b2176f68859ad080de1
Shreya Gupta <Shreya.Gupta@mrcooper.com>
2018-02-20 23:51:21 +0530

    installation

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "commonjs": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-const-assign": "warn",
+        "no-this-before-super": "warn",
+        "no-undef": "warn",
+        "no-unreachable": "warn",
+        "no-unused-vars": "warn",
+        "constructor-super": "warn",
+        "valid-typeof": "warn",
+        "no-extra-semi": "warn"
+    }
+};
--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
 import React, {Component} from 'react';
+import Dashboard from './Dashboard';
+
+[  
+    {  
+       "movie_title":"Avatar ",
+       "director_name":"James Cameron",
+       "actor_1_name":"CCH Pounder",
+       "actor_2_name":"Joel David Moore",
+       "genres":"Action|Adventure|Fantasy|Sci-Fi",
+       "language":"English",
+       "country":"USA",
+       "content_rating":"PG-13",
+       "budget":"237000000",
+       "title_year":"2009",
+       "plot_keywords":"avatar|future|marine|native|paraplegic",
+       "movie_imdb_link":"http://www.imdb.com/title/tt0499549/?ref_=fn_tt_tt_1"
+    },
+    {  
+       "movie_title":"Pirates of the Caribbean: At World's End ",
+       "director_name":"Gore Verbinski",
+       "actor_1_name":"Johnny Depp",
+       "actor_2_name":"Orlando Bloom",
+       "genres":"Action|Adventure|Fantasy",
+       "language":"English",
+       "country":"USA",
+       "content_rating":"PG-13",
+       "budget":"300000000",
+       "title_year":"2007",
+       "plot_keywords":"goddess|marriage ceremony|marriage proposal|pirate|singapore",
+       "movie_imdb_link":"http://www.imdb.com/title/tt0449088/?ref_=fn_tt_tt_1"
+    },
+    {  
+       "movie_title":"Spectre ",
+       "director_name":"Sam Mendes",
+       "actor_1_name":"Christoph Waltz",
+       "actor_2_name":"Rory Kinnear",
+       "genres":"Action|Adventure|Thriller",
+       "language":"English",
+       "country":"UK",
+       "content_rating":"PG-13",
+       "budget":"245000000",
+       "title_year":"2015",
+       "plot_keywords":"bomb|espionage|sequel|spy|terrorist",
+       "movie_imdb_link":"http://www.imdb.com/title/tt2379713/?ref_=fn_tt_tt_1"
+    },
+    {  
+       "movie_title":"The Dark Knight Rises ",
+       "director_name":"Christopher Nolan",
+       "actor_1_name":"Tom Hardy",
+       "actor_2_name":"Christian Bale",
+       "genres":"Action|Thriller",
+       "language":"English",
+       "country":"USA",
+       "content_rating":"PG-13",
+       "budget":"250000000",
+       "title_year":"2012",
+       "plot_keywords":"deception|imprisonment|lawlessness|police officer|terrorist plot",
+       "movie_imdb_link":"http://www.imdb.com/title/tt1345836/?ref_=fn_tt_tt_1"
+    },
 
 export default class App extends Component {
     render () {
         return (
             <div>
-                <h1> app </h1>
+                <Dashboard />
             </div>
         );
     }
